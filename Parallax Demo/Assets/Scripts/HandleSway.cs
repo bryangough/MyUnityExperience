cb5d57551c292636d5398841654121acb568a17e
Bryan Gough <gough.bryan@gmail.com>
2017-09-20 20:31:08 -0400

    Adding parallax example

---
+++ Parallax Demo/Assets/Scripts/HandleSway.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleSway : MonoBehaviour {
+	public float h = 0;
+	public float v = 0;
+    public float acceleration;
+	public float limit = 2.0f;
+    void Update () 
+	{
+			#if UNITY_IPHONE && !UNITY_EDITOR
+			h = Input.acceleration.x;
+			v = Input.acceleration.y;
+			acceleration = Input.acceleration.z;
+			#else
+			h = Input.GetAxis("Horizontal"); 
+			v = Input.GetAxis("Vertical");
+			#endif
+
+			//
+			//float translation = Time.deltaTime * 10 * h;
+        	//thisTransform.Translate(0, 0, translation);
+			if(transform.position.x>limit)
+			{
+				transform.position = new Vector3(limit, 0.0f, -10.0f);
+			}
+			if(transform.position.x<limit*-1)
+			{
+				transform.position = new Vector3(limit*-1.0f, 0.0f, -10.0f);
+			}
+			Vector3 pos = transform.position;
+			pos.x = limit * h;
+			transform.position = pos;
+			//transform.position += new Vector3(10 * Time.deltaTime * h, 0.0f, 0.0f);
+
+			
+			
+    }
+}
