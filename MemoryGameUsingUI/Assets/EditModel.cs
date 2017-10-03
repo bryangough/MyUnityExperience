c999fe706b4a9381b87ed261b52928b09ecb2b22
Bryan Gough <gough.bryan@gmail.com>
2017-10-03 18:00:18 -0400

    Adding edit abilities.

---
+++ MemoryGameUsingUI/Assets/EditModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditModel : MonoBehaviour {
+
+	public TimeModel currentModel;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	public void recieveModel(TimeModel time)
+	{
+
+	}
+	public void saveModel()
+	{
+
+	}
+}
