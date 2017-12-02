03f6b1cb380c99c24b27e440c7d061f55b58fd32
Bryan Gough <gough.bryan@gmail.com>
2017-12-02 07:32:05 -0500

    Split MOBA to it's own

--- MOBA/Assets/MOBA/Scripts/Spawning/SpawnDataOrder.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.Serializable]
-public class SpawnDataOrder  {
-	public GameObject unitPrefab;
-	public int number;
-}
